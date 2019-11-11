3c6142ba29775b174eef47661dca9eccf6039ac6
naghavi.ali <naghavi.ali@gmail.com>
2019-11-11 16:49:59 +0330

    '9-SpecialPythagoreanTriplet-Improved'

--- 9-SpecialPythagoreanTriplet.js
+++ 9-SpecialPythagoreanTriplet.js
@@ -9,3 +9,19 @@ function SpecialPythagoreanTriplet(){
                 }
 }
 SpecialPythagoreanTriplet();
+
+//Better solution
+
+function SpecialPythagoreanTriplet2(){
+    for(let i = 1; i < 1000; i++)
+        for(let j = i; j < 1000; j++){
+            let k = 1000 - i - j;
+            if(i * i + j * j == k * k && i + j + k === 1000){
+                console.log(i, j, k);
+                let result = [i, j, k];
+                return result.reduce((a, b) => a*b);
+            }
+        }
+}
+
+SpecialPythagoreanTriplet2();
